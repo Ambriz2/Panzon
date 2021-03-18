0522a19744048d59406e5b2974842a1029c1b0ab
Ulices Villegas <ulicesvillegas7@gmail.com>
2021-03-17 20:55:38 -0600

    Redux Form React

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider, provider } from 'react-redux';
-import { configureStore } from '.redux/configureStore';
+import { ConfigureStore } from '.redux/configureStore';
 import './App.css';
 
 const store = ConfigureStore();
