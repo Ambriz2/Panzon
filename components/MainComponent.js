8474eacbf3515646e858d4190d6da044c1a24dd8
unknown <ulicesvillegas7@gmail.com>
2021-04-21 19:52:32 -0600

    swipe and delete

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from './ContactComponent';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import Favorites from './FavoritesComponent';
 import SafeAreaView from 'react-native-safe-area-view';
 import Reservation from './ReservationComponent';
 import { connect } from 'react-redux';
@@ -145,6 +146,28 @@ const ReservationNavigator = createStackNavigator(
         })
     }
 );
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
 
 const CustomDrawerContentComponent = props => (
     <ScrollView>
@@ -207,6 +230,21 @@ const MainNavigator = createDrawerNavigator(
             )
         }
     },
+    
+Favorites: {
+    screen: FavoritesNavigator,
+    navigationOptions: {
+        drawerLabel: 'My Favorites',
+        drawerIcon: ({tintColor}) => (
+            <Icon
+                name='heart'
+                type='font-awesome'
+                size={24}
+                color={tintColor}
+            />
+        )
+    }
+},
     
         About: {
             screen: AboutNavigator,
